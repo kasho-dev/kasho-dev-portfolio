e2c8086c6012987593be9c1fe728a65e6348b314
kasho <159739498+kasho-dev@users.noreply.github.com>
2025-07-16 13:10:39 +0800

    - 📦 Imported SpeedInsights from vercel

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 </script>
 
 <template>
   <RouterView />
+  <SpeedInsights />
 </template>
